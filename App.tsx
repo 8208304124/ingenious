8ae510b527da2cc8044e8c52e6e54a53f3262e33
pratik-upperthrust <pratik.p@upperthrust.com>
2024-12-21 19:42:01 +0530

    Adding Splash screen

--- App.tsx
+++ App.tsx
@@ -5,15 +5,23 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RouteNavigater } from './src/routes/RoutNavigater';
 import ThemeProvider from './src/theme/themeProvider/ThemeProvider';
 // import { Provider } from "react-redux";
 import MultiLanguage from './src/utility/langSupport/MultiLanguage';
 import { Provider } from 'react-redux';
 import { store } from './src/store/store';
+import SplashScreen from 'react-native-splash-screen';
+import { Platform } from 'react-native';
 
 function App() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide();
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <MultiLanguage>
